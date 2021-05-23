fca42d27efe066e4d75742b81d2df115a587eb43
swarnikaraj <swarnikarajsingh@gamil.com>
2021-05-23 13:56:16 +0530

    Intial Commit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #
 **dconnect** is a lightweight data source connector wrapper for different databases, User can also perfom queries and will get data.
 #### Available datasources campactibility:
-*sqlLite, MySQL, SQL Server, Azure SQL DB, PostgreSQL, Redshift, Snowflake, Clickhouse, Oracle, IBM_Db, Vertica, MongoDB, AWS S3 Bucket, GoogleSheets, Elasticsearch, Google Analytics, Neo4j*
+*SQLite, MySQL, SQL Server, Azure SQL, PostgreSQL, Redshift, Snowflake, Clickhouse, Oracle, IBM_Db, Vertica, MongoDB, AWS S3 Bucket, GoogleSheets, Elasticsearch, Google Analytics, Neo4j*
 ### Installation
 #### Installation from Git
 Clone repository using below command
@@ -24,17 +24,30 @@ or
 python3 -m pip install dconnect
 ```
 ### Usages
+In order to use the dcoonect, use below steps
+1. Download [credentails.json](https://github.com/nit567esh/dconnect/blob/master/credentails.json) file.
+2. Configure the required connection using the credentails.json template.
+3. Store it somewhere in safe folder.
+4. Use below commands to run you dconnect
+```sh
+>>> import dconnect as c
+>>> conids = c.conids('path_of_your_saved_credentails.json')
+>>> print(conids) # Choose any connection id from list to validate the connection
+>>> con = c.connect('your_connection_id(dsidentifier key in credentails.json)')
+>>> df  = c.runsql(con, 'show tables;')
+```
 **Functions:**
-*1. See available connection IDs*
+1. List configured connection IDs
 ```sh
->>> import connection as c
->>> c.conids()
+>>> import dconnect as c
+>>> conids = c.conids('path_of_your_saved_credentails.json')
+>>> print(conids)
 ```
-*2. Connect using a conid*
+2. Connect using a conid
 ```sh
->>> con = c.connect('<conid-string>')
+>>> con = c.connect('<your_connection_id>')
 ```
-*2. Run SQL quries incase on Relational DBs/DWs connection object & get data into pandas*
+2. You can only run SQL queries incase on Relational DBs/DWs connection object.
 ```sh
 >>> df = c.runsql(con,'<Sql-Query>')
 ```
@@ -44,7 +57,28 @@ python3 -m pip install dconnect
 >>> con = c.connect('redshift_<host>_<database>')
 >>> df = c.runsql(con,'select top 10 * from edw.dim_product')
 ```
-**Note** - Same is applicalble for the following list of DBs: *MySQL, SQL Server, Azure SQL DB, PostgreSQL, Redshift* 
+**Note** - Same is applicalble for the following list of DBs: *SQLite, MySQL, SQL Server, Azure SQL, PostgreSQL, Redshift, Snowflake, Clickhouse, Oracle, IBM_Db, Vertica* 
+
+**2. For Others**
+Function **runsql** is only applicable to relational DBs/DWs. For other data source dconnect only allow to connect with data source and for query purpose you can ues the respective generic packages using the connection object initilized by dconnect.
+
+*Google Sheet Example*
+```sh
+>>> con = c.connect('gsheet_<username>@gamil.com') # using dconnect
+>>> sh = con.open("<GsheetName>") # using pysgsheets
+>>> wks = sh.worksheet_by_title("<GsheetTab>")  # using pysgsheets
+>>> df = wks.get_as_df()  # using pysgsheets
+```
+
+| DataSource|Pythob Pkg|GitRepo|Pypi|Dependencies|
+|:---------:|:--------:|:-----:|:--:|:------:|
+|Google Sheet|pygsheets| https://github.com/nithinmurali/pygsheets  | https://pygsheets.readthedocs.io/en/stable/ |client_secret json file|
+|Google Analytics|google2pandas| https://github.com/panalysis/Google2Pandas  | https://pypi.org/project/Google2Pandas/ |client_secret json file, token file|
+|GoogleAds|googleads| https://github.com/googleads/googleads-python-lib  | https://pypi.org/project/googleads/ |client_secret json file|
+|AWS S3|boto3| https://github.com/boto/boto3  | https://pypi.org/project/boto3/ ||
+|Mongo DB|pymongo| https://github.com/mher/pymongo  | https://pypi.org/project/pymongo/ ||
+|Elasticsearch|elasticsearch| https://github.com/elastic/elasticsearch  | https://pypi.org/project/elasticsearch/ ||
+|Neo4j|neo4j| https://github.com/neo4j  | https://pypi.org/project/neo4j/ ||
 
 ### Todos
- - Google Adwords, Mixpanel, Sisense integrations and many more
+ - REST API, FTP, SFTP, Google Adsense/Manager/Search Console, Mixpanel, Hive, HDFC, Apache Kylin, Apache Druid and many more
--- build/lib/dconnect/dconnect.py
+++ build/lib/dconnect/dconnect.py
@@ -15,31 +15,44 @@ def conids(path=None):
 def connect(conid=None):
     df = pandas.json_normalize(data[str(conid).split('_')[0]])
     df = (df[(df['dsidentifier'] == conid)]).reset_index()
-    
-    if conid.split('_')[0].lower() in ('redshift', 'postgresql'):
-        import psycopg2
-        con = psycopg2.connect("dbname='"+str(df['db'][0])+"' port='"+str(df['port'][0])+"' user='"+str(df['user'][0])+"' password='"+str(df['pswd'][0])+"' host='"+str(df['host'][0])+"'")       
-    
-    elif conid.split('_')[0].lower() in ('snowflake'):
-        import snowflake.connector
-        con = snowflake.connector.connect(user=df['user'][0], password=df['password'][0], account=df['account'][0], warehouse=df['warehouse'][0], database=df['database'][0],)
-        
-    elif conid.split('_')[0].lower() in ('clickhouse'):
-        from clickhouse_driver import Client
-        con = Client(database=df['db'][0], host=df['host'][0], port=df['port'][0], user=df['user'][0], password=df['pswd'][0])        
-        
+
+    if conid.split('_')[0].lower() in ('mysql'):
+        import pymysql
+        con = pymysql.connect(host=str(df['host'][0]),port=int(df['port'][0]),user=str(df['user'][0]),password=str(df['pswd'][0]),db=str(df['db'][0]))
+#
     elif conid.split('_')[0].lower() in ('azuresql', 'sqlserver'):
         import pyodbc
         con = pyodbc.connect(driver=str(df['driver'][0]),server=str(df['host'][0])+','+str(df['port'][0]),database=str(df['db'][0]),uid=str(df['user'][0]),pwd=str(df['pswd'][0]))
-    
-    elif conid.split('_')[0].lower() in ('mysql'):
-        import pymysql
-        con = pymysql.connect(host=str(df['host'][0]),port=int(df['port'][0]),user=str(df['user'][0]),password=str(df['pswd'][0]),db=str(df['db'][0]))
-    
+#    
+    elif conid.split('_')[0].lower() in ('redshift', 'postgresql'):
+        import psycopg2
+        con = psycopg2.connect("dbname='"+str(df['db'][0])+"' port='"+str(df['port'][0])+"' user='"+str(df['user'][0])+"' password='"+str(df['pswd'][0])+"' host='"+str(df['host'][0])+"'")       
+#    
+    elif conid.split('_')[0].lower() in ('snowflake'):
+        import snowflake.connector
+        con = snowflake.connector.connect(user=df['user'][0], password=df['password'][0], account=df['account'][0], warehouse=df['warehouse'][0], database=df['database'][0],port=df['port'][0],)
+#        
+    elif conid.split('_')[0].lower() in ('clickhouse'):
+        from clickhouse_driver import Client
+        con = Client(database=df['db'][0], host=df['host'][0], port=df['port'][0], user=df['user'][0], password=df['pswd'][0])        
+#
+    elif conid.split('_')[0].lower() in ('presto'):
+        import prestodb
+        con = prestodb.dbapi.connect(host=df['host'][0], port=df['port'][0], user=df['user'][0], catalog=df['catalog'][0], http_scheme=df['http_scheme'][0], auth=prestodb.auth.BasicAuthentication(df['principal_id'][0], df['pswd'][0]),)
+#        
+    elif conid.split('_')[0].lower() in ('ibm_db'):
+        import ibm_db
+        con = ibm_db.connect("DATABASE="+df['db'][0]+";HOSTNAME="+df['host'][0]+";PORT="+df['port'][0]+";PROTOCOL="+df['protocol'][0]+";UID="+df['user'][0]+"; PWD="+df['pswd'][0]+";")
+#
+    elif conid.split('_')[0].lower() in ('oracle'):
+        import cx_Oracle
+        dsn_tns = cx_Oracle.makedsn(df['host'][0], df['port'][0], service_name=df['service_name'][0])
+        con = cx_Oracle.connect(user=df['user'][0], password=df['pswd'][0], dsn=dsn_tns)
+#        
     elif conid.split('_')[0].lower() in ('mongodb'):
         from pymongo import MongoClient
         con = MongoClient(str(df['url'][0]))
-        
+#        
     elif conid.split('_')[0].lower() in ('gsheet'):
         import pygsheets
         pwd = os.getcwd()
@@ -48,7 +61,7 @@ def connect(conid=None):
         os.chdir(path)
         con = pygsheets.authorize(file)
         os.chdir(pwd)        
-        
+#        
     elif conid.split('_')[0].lower() in ('googleanalytics'):
         import google2pandas
         pwd = os.getcwd()
@@ -58,7 +71,7 @@ def connect(conid=None):
         os.chdir(path)
         con = google2pandas.GoogleAnalyticsQuery(secrets=file, token_file_name=token_file)
         os.chdir(pwd)
-        
+#        
     elif conid.split('_')[0].lower() in ('googleads'):
         import sys,locale,_locale
         from googleads import adwords
@@ -69,37 +82,30 @@ def connect(conid=None):
         os.chdir(path)
         con = adwords.AdWordsClient.LoadFromStorage(file)
         os.chdir(pwd)
-
+#
     elif conid.split('_')[0].lower() in ('awss3'):
         import boto3
         con = boto3.client('s3', aws_access_key_id=str(df['access_key'][0]),aws_secret_access_key=str(df['secret_access_key'][0]))
-    
-    elif conid.split('_')[0].lower() in ('ftp'):
-        import pysftp
-        from ftplib import FTP
-        con = FTP(str(df['host'][0]))
-        con.login(user=df['user'][0], passwd = df['pswd'][0])
-    
-    elif conid.split('_')[0].lower() in ('sftp'):
-        cnopts = pysftp.CnOpts()
-        cnopts.hostkeys = None
-        con = pysftp.Connection(str(df['host'][0]), username=str(df['user'][0]),password=str(df['pswd'][0]),port=int(df['port'][0]),cnopts=cnopts)
-    
+        
     elif conid.split('_')[0].lower() in ('elasticsearch'):
         import elasticsearch
         if str(df['security'][0])=='http':
             con = elasticsearch.Elasticsearch(str(df['host'][0]),http_auth=(str(df['user'][0]), str(df['pswd'][0])),port=int(df['port'][0]),)
         elif str(df['security'][0])!='https':
             con = elasticsearch.Elasticsearch(str(df['host'][0]),http_auth=(str(df['user'][0]), str(df['pswd'][0])),port=int(df['port'][0]),use_ssl=True,verify_certs=True,ca_certs=certifi.where(),)
-
+#
     elif conid.split('_')[0].lower() in ('neo4j'):
         import neo4j
         con = neo4j.GraphDatabase.driver(str(df['uri'][0]), auth=(str(df['user'][0]), str(df['pswd'][0])))
-
-    elif conid.split('_')[0].lower() in ('restapisearchmetric'):
-        response = requests.post(str(df['endpoint'][0]), data={'grant_type': 'client_credentials'},auth=(str(df['api_key'][0]), str(df['api_secret'][0])))
-        con = json.loads(response.content)
-        con = str(con['access_token'])
+#    
+    elif conid.split('_')[0].lower() in ('vertica'):
+        import vertica_python
+        conn_info = {'host': df['host'][0],
+                     'port': df['port'][0],
+                     'user': df['user'][0],
+                     'password': df['pswd'][0],
+                     'database': df['db'][0]}
+        con = vertica_python.connect(**conn_info)
         
     return con
 
